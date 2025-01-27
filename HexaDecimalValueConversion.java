bb048b3e384e62412b23f0e13a1c531ccc384d41
Raja-Rajeshwaran <rajarajeshwaran2004@gmail.com>
2025-01-27 23:03:25 +0530

    Modified

--- HexaDecimalValueConversion.java
+++ HexaDecimalValueConversion.java
@@ -7,18 +7,19 @@ public class HexaDecimalValueConversion {
         try (Scanner s = new Scanner(System.in)) {
             String hex = s.nextLine();
 
-            //Hexadecimal value to Integer
+            // Hexadecimal value to Integer
 
-            if (hex.startsWith("0x") || hex.startsWith("0X")) {
-                long decimal = Long.decode(hex);
+            if (hex.startsWith("0x") || hex.startsWith("0X")) {  // endsWith (or) startsWith method
+                long decimal = Long.decode(hex);     // decode method
                 System.out.println(decimal);
             }
             long decimalValue = Long.parseLong(hex.substring(2), 16);
             System.out.println(decimalValue);
 
-            //Number to Hexadecimal value
+            // Number to Hexadecimal value
+
             Long decimal2 = s.nextLong();
-            String hexvalue = Long.toHexString(decimal2);
+            String hexvalue = Long.toHexString(decimal2);   // toHexString method
             System.out.println("0x"+hexvalue);
 
         }
