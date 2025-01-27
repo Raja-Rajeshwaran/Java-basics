13cd653a1b819f9ee8e3e86d080b34354357c7f1
Raja-Rajeshwaran <rajarajeshwaran2004@gmail.com>
2025-01-27 23:19:34 +0530

    Modified

---
+++ Print Formats/Basic_Print_Formats.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class Print {
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+            // Escape sequence \"
+            System.out.print("Success is when your \"signature\" becomes \"autograph\"");
+
+            // Print up to 2 decimals
+            double decimal = sc.nextDouble();
+            System.out.printf("%.2f", decimal);
+
+            // Print with 20 width space 
+            System.out.printf("%20.4f\n", decimal);
+
+            // Print with desired width space
+            int i = sc.nextInt();
+            System.out.printf("%." + i + "f", decimal);
+            System.out.println(Math.round(decimal));
+
+        }
+    }
+}
