b2776a5e82a4ad7bde24fa0b2fbb9d18c92b2551
coder-raja <rajarajeshwaran2004@gmail.com>
2025-02-07 23:48:57 +0530

    Commit

--- Basic Conversion/com/conversion/Decimal_Exponential_Conversion.java
+++ Basic Conversion/com/conversion/Decimal_Exponential_Conversion.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Decimal_Exponential_Conversion {
 
     public static void main(String[] args) {
-        
+
         try (Scanner s = new Scanner(System.in)) {
             double d = s.nextDouble();
             System.out.printf("%e%n", d);
